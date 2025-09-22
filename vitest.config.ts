c9d362741b7b14ea105bf0d4c12e61ecd1244bca
Thilo <125662076+thilo-m@users.noreply.github.com>
2025-09-22 13:20:03 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,8 +9,9 @@ export default defineConfig({
 		exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
 		coverage: {
 			provider: 'v8',
-			reporter: ['text', 'json', 'html'],
+			reporter: ['text', 'json', 'html', 'lcov'],
 			include: ['nodes/**/*.ts', 'credentials/**/*.ts'],
+			reportsDirectory: './coverage',
 			exclude: [
 				'node_modules/',
 				'dist/',
